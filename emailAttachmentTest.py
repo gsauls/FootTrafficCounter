5927ed7f61a1e733e0f4ca53d98b450aa581b1a9
Garrett Sauls <garrettsauls00@gmail.com>
2021-07-13 14:40:48 -0500

    small changes

--- emailAttachmentTest.py
+++ emailAttachmentTest.py
@@ -6,20 +6,21 @@ from email.mime.application import MIMEApplication
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
 
-gmail = 'piraspberry668@gmail.com'
-password = 'Raspberry123!'
+gmail = 'sender email'
+password = 'sender password'
 
 message = MIMEMultipart('mixed')
 message['From'] = 'Contact <{sender}>'.format(sender = gmail)
-message['To'] = 'gsauls3421@gmail.com'
-message['CC'] = 'gsauls3421@gmail.com'
+message['To'] = 'reciever email'
+message['CC'] = 'cc reciever email'
 message['Subject'] = 'Hello'
 
 msg_content = '<h4> Hi there, <br> This is a testing message.</h4>\n'
 body = MIMEText(msg_content, 'html')
 message.attach(body)
 
-attachmentPath = "/home/pi/Desktop/Hive Foot Traffic Data 06.17.21.png"
+
+attachmentPath = "/home/pi..."
 
 try:
     with open(attachmentPath, "rb") as attachment:
